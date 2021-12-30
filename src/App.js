22948a0e6d6e0d72c1ae9deacd16fb721ba94a10
aminlahiani <aminlahiani@outlook.com>
2021-12-30 14:19:42 +0100

    fix routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from "./pages/Services";
+import News from "./pages/News";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -10,6 +14,10 @@ export default function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="nwes" element={<News />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
